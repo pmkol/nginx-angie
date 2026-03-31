6da21cecdab1d484426907d1e7afa6cb5861bcbb
pmkol <pmkol@users.noreply.github.com>
2026-04-01 01:27:18 +0800

    Version bump.

--- src/core/nginx.h
+++ src/core/nginx.h
@@ -10,8 +10,8 @@
 #define _NGINX_H_INCLUDED_
 
 
-#define nginx_version      1028002
-#define NGINX_VERSION      "1.28.2"
+#define nginx_version      1028003
+#define NGINX_VERSION      "1.28.3"
 #define NGINX_VER          "nginx/" NGINX_VERSION
 
 #define NGINX_VER_BUILD    NGINX_VER
